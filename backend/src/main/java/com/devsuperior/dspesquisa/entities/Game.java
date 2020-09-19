4efd8ef3244a98f5647c7030719c2c5e1e51fc97
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2020-09-19 16:13:38 -0300

    Criando entidades iniciais e relacionamentos.

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -0,0 +1,87 @@
+package com.devsuperior.dspesquisa.entities;
+
+import com.devsuperior.dspesquisa.entities.enums.Platform;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game")
+public class Game implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Platform platform;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    @OneToMany(mappedBy = "game")
+    private List<Record> records = new ArrayList<>();
+
+    public Game() {
+    }
+
+    public Game(Long id, String nome, Platform platform, Genre genre) {
+        this.id = id;
+        this.nome = nome;
+        this.platform = platform;
+        this.genre = genre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Platform getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(Platform platform) {
+        this.platform = platform;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public List<Record> getRecords() {
+        return records;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game)) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
