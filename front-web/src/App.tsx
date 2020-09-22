e810f6cf36c6976168c0c3efbcae4fb309563d6c
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2020-09-21 23:26:59 -0300

    Testando mensagens

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Hello from './Hello';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello semana devSuperior</h1>
+      <div>
+        <Hello message="Lucas"/>
+        <Hello message="Ana"/>
+        <Hello message="Nelio"/>
+        <Hello message="Washington"/>
+      </div>
     </div>
   );
 }
