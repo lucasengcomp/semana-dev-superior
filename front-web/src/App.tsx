eac9cdf6ad6ce77727082f483d707c15c00e32ac
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2020-09-25 22:30:40 -0300

    Correção nome da plataforma.

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import Hello from './Hello';
+import Header from './components';
 
 function App() {
   return (
     <div className="App">
       <div>
-        <Hello message="Lucas"/>
-        <Hello message="Ana"/>
-        <Hello message="Nelio"/>
-        <Hello message="Washington"/>
+        <Header/>
       </div>
     </div>
   );
