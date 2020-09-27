15675b8e51e5a30a6b937beb15de85bb4dbb7add
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2020-09-27 00:05:44 -0300

    Front-mobile criado

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'https://localhost:8080';
+const BASE_URL = 'http://localhost:8080';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<barChartData[]>([]);
